f07f40e2e6d6c1f55173829cd67bdf805b61f1e2
IsaacEscobar <isak.escobarr@gmail.com>
2024-08-08 19:33:44 -0500

    Agregado escenario de juego1 y cambio de escenario desde el menu
    Se agregó el escenario para la primera actividad y se cambia la funcionalidad del menu para cambiar de escenario en lugar de mover la camara de posicion

--- PruebaMR-PDG/Assets/ChangeScenario.cs
+++ PruebaMR-PDG/Assets/ChangeScenario.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScenario : MonoBehaviour
 {
-    public void changePosition()
+    public void changeScenarioMainMenu()
     {
-        transform.position = transform.position - new Vector3(60f,0f,0f);
+        SceneManager.LoadScene("EscenarioInicial");
+    }
+    public void changeScenarioGame1()
+    {
+        //transform.position = transform.position - new Vector3(60f,0f,0f);
+        SceneManager.LoadScene("EscenarioJuego1");
     }
     // Start is called before the first frame update
     void Start()
